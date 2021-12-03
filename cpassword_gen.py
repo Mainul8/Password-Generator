1cd99b81a8b72e47c8c6a619b7f0bc96af68cff8
Sk Mainul Islam <94590121+Mainul8@users.noreply.github.com>
2021-12-03 11:09:14 +0530

    Add files via upload

---
+++ cpassword_gen.py
@@ -0,0 +1,52 @@
+import random
+
+letters = ["A" ,"B" , "C" , "D" ,"E", "F","G" ,"H","I" ,"J" ,"K" ,"L",
+"M", "N" ,"O" , "P" ,"Q" , "R" , "S", "T" , "U", "V" , "W" ,"X" ,"Y" ,"Z",
+"a" , "b", "c" , "d" , "e", "f" , "g" "h", "i", "j","k" ,"l","m", "n" ,"o",
+"p" ,"q" ,"r" , "s" , "t" , "u", "v", "w", "x" , "y","z"]
+
+numbers = ["0","1","2","3","4","5","6","7","8","9"]
+
+symbols = ["!","@","#","$","%","^","&","*","?","+","="]
+
+print("WELCOME TO THE PASSWORD GENERATOR!!!!")
+nr_letters = int(input("How many letters do you want to add:\n"))
+nr_numbers = int(input("How many numbers do you want to add:\n"))
+nr_symbols = int(input("How many symbols do you want to add:\n"))
+
+#Easy level
+#password = ""
+
+#for char in range (1 , nr_letters + 1):
+    #random_char = random.choice(letters)
+    #password += random_char
+    #password += random.choice(letters)
+
+#for char in range (1 , nr_numbers + 1):
+    #password += random.choice(numbers)
+#for char in range (1 , nr_symbols + 1):
+    #password += random.choice(symbols)
+
+#print(password)
+
+#Hard level
+
+password_list = []
+
+for char in range (1 , nr_letters + 1):
+    #random_char = random.choice(letters)
+    #password += random_char
+    password_list.append(random.choice(letters))
+
+for char in range (1 , nr_numbers + 1):
+    password_list += random.choice(numbers)
+
+for char in range (1 , nr_symbols + 1):
+    password_list += random.choice(symbols)
+#print(password_list)
+random.shuffle(password_list)
+#print(password_list)
+password = ""
+for char in password_list:
+    password += char
+print(f"Your strong password is : {password}")
